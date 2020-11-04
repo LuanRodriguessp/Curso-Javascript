6d0e4e0e44540cafea52dead4b79e6b87eddf249
Luan Rodrigues <luan.rodrigues.souza@icloud.com>
2020-11-04 11:22:46 -0300

    Edit ex006

--- aula11/ex008.js
+++
@@ -1,8 +0,0 @@
-let vel = 70.5 // variavel com a velocidade
-
-    console.log(`a velocidade do seu carro é ${vel} KM/H`)
-
-if (vel > 60 ) { //condição simples, se o valor da variavel velocidade for maior que 60, executar o console.log. 
-    console.log('voce ultrapassou a velocidade permitida, foi mutado')  // se o valor da variavel for maior que 60 vai printar o texto no console
-}
-    console.log('Diriga sempre usando cinto de segurança')
--- aula11/ex009.js
+++
@@ -1,8 +0,0 @@
-let pais = 'EUA' // variavel com o pais
-console.log (`vivendo em ${pais}`) // printa no console o texto.
-if (pais == 'EUA') {      // se o pais for igual a EUA, ele vai printar a msg do console.
-    console.log('Estrangeiro') // printa a msg caso o pais seja igual a EUA
-} 
-else {  // se não, printa a msg do console log abaixo de else.
-    console.log('Brasileiro')
-}
